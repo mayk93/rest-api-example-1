d702699a33f512b332ccea03eecf16452c952545
michael <mihai@mandrescu.co>
2020-05-26 16:16:25 +0100

    Test and implementation of admin change to support custom user model

---
+++ app/core/tests/tests_admin.py
@@ -0,0 +1,35 @@
+from django.test import TestCase, Client
+from django.urls import reverse
+from django.contrib.auth import get_user_model
+
+admin_email = 'admin@test.com'
+admin_password = 'password'
+
+regular_email = 'regular@test.com'
+regular_password = 'password'
+regular_name = 'Name'
+
+
+class AdminTests(TestCase):
+    def setUp(self) -> None:
+        self.client = Client()
+
+        self.admin_user = get_user_model().objects.create_superuser(
+            email=admin_email,
+            password=admin_password
+        )
+        self.regular_user = get_user_model().objects.create_superuser(
+            email=regular_email,
+            password=regular_password,
+            name=regular_name
+        )
+
+        self.client.force_login(self.admin_user)
+        self.client.force_login(self.admin_user)
+
+    def test_users_listed(self):
+        url = reverse('admin:core_user_changelist')
+        response = self.client.get(url)
+
+        self.assertContains(response, self.regular_user.name)
+        self.assertContains(response, self.regular_user.email)
